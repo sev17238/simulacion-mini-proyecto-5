b13046c0ded14064e0f0b360b1f6405f925394ee
Diego Sevilla <sevilla.diego8@gmail.com>
2020-11-03 22:43:17 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+###################################################
+# Guatemala, noviembre del 2020
+###################################################
+# Modelacion y simulacion
+# Diego Sevilla 17238
+# Alejandro Tejada 17584
+###################################################
+
+# Refs:
+# https://gist.github.com/ryangmolina/e1c87509b6919ac8aaf3eceb315d3e5e
+
+import numpy as np
+import random
+import math
+import skfuzzy as fuzz
+
+
